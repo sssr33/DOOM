a443fcdc19a59689c401dd770c2d3c2a159bc412
Alexander Chaikovsky <sskr33@gmail.com>
2019-12-31 16:40:50 +0200

    i_net compile

---
+++ vs/DoomApp/DoomCore/compat/netinet/in.h
@@ -0,0 +1,13 @@
+#pragma once
+
+// https://unix.superglobalmegacorp.com/Net2/newsrc/netinet/in.h.html
+
+/*
+ * Local port number conventions:
+ * Ports < IPPORT_RESERVED are reserved for
+ * privileged processes (e.g. root).
+ * Ports > IPPORT_USERRESERVED are reserved
+ * for servers, not necessarily privileged.
+ */
+#define	IPPORT_RESERVED		1024
+#define	IPPORT_USERRESERVED	5000
---
+++ vs/DoomApp/DoomCore/compat/sys/socket.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN
+#include <time.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdio.h>
+
+#define ioctl ioctlsocket
