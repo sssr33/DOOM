a56dc62dc4b0da5282243cc9dec1f19269839737
Alexander Chaikovsky <sskr33@gmail.com>
2024-01-01 00:48:13 +0200

    C++20 for projects, missing type specifier warning fixes

--- vs/DoomApp/DoomCore/compat/windowsSafe.h
+++ vs/DoomApp/DoomCore/compat/windowsSafe.h
@@ -1,7 +1,30 @@
 #pragma once
 
+// DOOM defines SHORT and LONG as macros so push/pop them to fix warnings
+#if defined(SHORT)
+#pragma push_macro("SHORT")
+#define RESTORE_SHORT
+#undef SHORT
+#endif
+
+#if defined(LONG)
+#pragma push_macro("LONG")
+#define RESTORE_LONG
+#undef LONG
+#endif
+
 // https://stackoverflow.com/questions/21399650/cannot-include-both-files-winsock2-windows-h
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <Windows.h>
 #undef LoadMenu
+
+#if defined(RESTORE_SHORT)
+#undef RESTORE_SHORT
+#pragma pop_macro("SHORT")
+#endif
+
+#if defined(RESTORE_LONG)
+#undef RESTORE_LONG
+#pragma pop_macro("LONG")
+#endif
