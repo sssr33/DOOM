10321accd28a9a00859212bfa57e3035b854fcc8
Alexander Chaikovsky <sskr33@gmail.com>
2023-12-31 21:42:10 +0200

    setting env variable for doom

--- vs/DoomApp/DoomApp/Wrapper.cpp
+++ vs/DoomApp/DoomApp/Wrapper.cpp
@@ -6,9 +6,31 @@ extern "C" {
 #include <d_main.h>
 }
 
+#include <string>
+#include <cassert>
+
 void mainWrapper(char **argv, int argc) {
 	myargc = argc;
 	myargv = argv;
+
+	{
+		errno_t err = {};
+		WCHAR path[MAX_PATH];
+		GetModuleFileNameW(NULL, path, MAX_PATH);
+
+		std::wstring pathStr = path;
+		pathStr = pathStr.substr(0, pathStr.find_last_of(L"\\/"));
+
+		err = _wputenv_s(L"HOME", pathStr.c_str());
+		if (err != 0) {
+			assert(false);
+		}
+
+		err = _wputenv_s(L"DOOMWADDIR", pathStr.c_str());
+		if (err != 0) {
+			assert(false);
+		}
+	}
 
 	D_DoomMain();
 }
